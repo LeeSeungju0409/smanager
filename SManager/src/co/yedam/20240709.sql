657a42b67d24757cf7be873c4e340b2d202e5454
admin <admin@YD02-07>
2024-07-09 17:51:53 +0900

    테이블쿼리추가함.

---
+++ SManager/src/co/yedam/20240709.sql
@@ -0,0 +1,7 @@
+--- jsp / jsp
+
+create user jsp identified by jsp; -- jsp/jsp
+grant connect, resource to jsp; -- jsp: connect, resource ±ÇÇÑ.
+alter user jsp default tablespace users quota unlimited on users; -- table
+
+
