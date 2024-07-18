b5defa25d70910ff6ff71efa15d408a9a601369a
admin <admin@YD02-07>
2024-07-18 17:52:23 +0900

    Board0718

---
+++ BoardWeb/src/main/java/com/yedam/service/BoardService.java
@@ -0,0 +1,36 @@
+package com.yedam.service;
+
+import java.util.List;
+
+import com.yedam.vo.BoardVO;
+
+/*
+ * MCV 패턴의 디자인에 따라서 Model영역(service, dao:mapper).
+ * 책: 4강 MVC기반 웹프로젝트 참고.
+ * 숙제: 536페이지 MVC에 자세히 읽고 오세요.
+ * 
+ * 서비스부분 없으면...
+ * dao)
+ * 등록 -> 수정 -> 조회
+ * method) add + mod + select;
+ * 수정 -> 등록 -> 조회
+ * method) mod + add + select;
+ * 조회 -> 수정 -> 등록
+ * method) select + mod + add;
+ * 
+ * 
+ * 서비스부분 있으면...
+ * service)
+ * add + mod + select; 1줄
+ * dao)
+ * method) add;
+ * method) mod;
+ * method) select;
+ */
+public interface BoardService {
+	List<BoardVO> boardList();
+	boolean addBoard(BoardVO board);
+	boolean modifyBoard(BoardVO board);
+	boolean removeBoard(int boardNo);
+	BoardVO getBoard(int boardNo);
+}
--- BoardWeb/src/test/java/com/yedam/common/AppTest.java
+++ BoardWeb/src/test/java/com/yedam/common/AppTest.java
@@ -1,16 +1,46 @@
 package com.yedam.common;
 
-import org.apache.ibatis.session.SqlSession;
 
-import com.yedam.mapper.StudentMapper;
+import com.yedam.service.BoardService;
+import com.yedam.service.BoardServiceImpl;
 
 public class AppTest {
 	public static void main(String[] args) {
-		SqlSession sqlSession = //
-				DataSource.getInstance().openSession();
-		StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+		BoardService svc = new BoardServiceImpl();
+		svc.boardList().forEach(System.out::println);
+		System.out.println("- End -");
+//		SqlSession sqlSession = //
+//				DataSource.getInstance().openSession(true); //자동으로 커밋하려고 true 넣음.
+//		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+//		
+//		BoardVO brd = new BoardVO();
+////		brd.setTitle("매퍼테스트1111");
+//		brd.setContent("조건이 제대로 되는지2222");
+//		brd.setWriter("newbie");
+//		brd.setBoardNo(5);
+//		
+////		if(mapper.insertBoard(brd)==1) {
+////			System.out.println("OK");
+////		}
+//		
+//		if(mapper.updateBoard(brd)==1) {
+//			System.out.println("OK");
+//		}
+//		
+//		if(mapper.deleteBoard(brd.getBoardNo())==1) {
+//			System.out.println("OK");
+//		}
+//	
+//		
+//		System.out.println(brd.getBoardNo());
+//		
+//		mapper.selectList().forEach(board -> System.out.println(board.toString()));
+//		
+//		System.out.println("- End -");
 		
-		mapper.studentList().forEach(student -> {System.out.println(student);});
 		
+
+
+			
 	}
 }
